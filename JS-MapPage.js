f0f7dc35693e2690dcf68282de7b54b0deb22039
EnesiMe <71355013+EnesiMe@users.noreply.github.com>
2021-04-10 13:17:47 -0400

    Add files via upload

---
+++ JS-MapPage.js
@@ -0,0 +1,7 @@
+var map = tt.map({
+    key: '<FhrOUyUQxQAaFfOZHgwPk9Eiun5HXIMp>',
+    container: 'map',
+    dragPan: !isMobileOrTablet()
+});
+map.addControl(new tt.FullscreenControl());
+map.addControl(new tt.NavigationControl());
